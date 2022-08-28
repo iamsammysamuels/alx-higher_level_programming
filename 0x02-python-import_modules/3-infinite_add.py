54985c2007ff74a22179cf274d1b53bc104eed39
iamsammysamuels <iamsammysamuels@gmail.com>
2022-08-28 05:57:36 -0700

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,28 +1,12 @@
-"""from sys import argv
+#!/usr/bin/python3
+from sys import argv
 if __name__ != "__main__":
     exit(0)
 result = 0
-argc = len(argv) - 1
 i = 0
-if argc != 0:
-    for num in argv:
-        if i != 0:
-            result += int(num)
-        else:
-            i += 1
-    print("{:d}".format(result))"""
-
-#!/usr/bin/python3
-import sys
-
-if __name__ != "__main__":
-    exit()
-
-i = 0
-result = 0
-for argument in sys.argv:
+for num in argv:
     if i != 0:
-        result += int(argument)
+        result += int(num)
     else:
         i += 1
 print("{:d}".format(result))
