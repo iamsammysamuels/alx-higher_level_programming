444d42859eb89b1133419b33005e0fcab2d396b2
iamsammysamuels <iamsammysamuels@gmail.com>
2022-08-31 21:05:30 -0700

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    """
+        Executes the square value of all integers of a matrix
+    """
     return ([[m**2 for m in row] for row in matrix])
