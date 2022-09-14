300d5839888b79d5ba8cb9d634548636eac81d48
iamsammysamuels <iamsammysamuels@gmail.com>
2022-09-13 20:54:23 -0700

    Python - Classes and Objects

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    A class Square that defines a square.
+
+    Attribues:
+        size: The size of the square.
+    """
+    def __init__(self, size=0):
+        """
+        The __init__ method for Square class
+
+        Args:
+            size: (:obj: 'int', optional): A private instance size
+        Error Raises:
+            TypeError: Exception if size is not an integer.
+            ValueError: Exception if size is less than 0.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    Class Square that has attributes. Instantiation with size
+
+    Attributes:
+        size (int): The size of the square
+    """
+    def __init__(self, size=0):
+        """
+        The __init__ method for Square class
+        Args:
+            size: (:obj: 'int', optional): A private instance size
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        Call the function to checking property
+        Returns:
+            The size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size ** 2
