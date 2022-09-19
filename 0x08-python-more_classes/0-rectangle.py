b79584a39dbe71c933d68af285cb1d28a3f718d1
iamsammysamuels <iamsammysamuels@gmail.com>
2022-09-19 13:21:25 -0700

    Python - More Classes and Objects

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -2,6 +2,8 @@
 
 """A module with an empty class Rectangle that defines a rectangle
 """
+
+
 class Rectangle:
     """A class that defines a rectangle
     """
--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,20 +3,21 @@
 """A module with a class Rectangle that defines a rectangle
 """
 
+
 class Rectangle:
     """Defines a rectangle with height and width.
     """
 
     def __init__(self, width=0, height=0):
         """Initialises the rectangle with the arguments values.
-        
+
         Attributes:
             width: (:obj: 'int', optional): height of the rectangle.
             height: (:obj 'int', optional): width of the rectangle.
         """
         self.__width = width
         self.__height = height
-    
+
     @property
     def width(self):
         """Gets the argument.
@@ -31,7 +32,8 @@ class Rectangle:
         """Checks errors and sets for size attribute
 
         Attributes:
-            value: (:obj: 'int') value to assign to the object after error checks.
+            value: (:obj: 'int') value to assign
+            to the object after error checks.
 
             Raises:
                 TypeError: Exception if width is not an integer.
@@ -39,7 +41,7 @@ class Rectangle:
         """
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        
+
         elif value < 0:
             raise ValueError("width must be >= 0")
 
@@ -49,7 +51,7 @@ class Rectangle:
     @property
     def height(self):
         """Gets the value of height
-        
+
         Returns:
             The height of the rectangle
         """
@@ -60,7 +62,8 @@ class Rectangle:
         """Sets the value of the object with value attribute
 
         Attribute:
-            value: (:obj: 'int') value to assign to the object after error checks. 
+            value: (:obj: 'int') value to
+            assign to the object after error checks.
 
         Raises:
             TypeError: Exception if the value is not an int.
@@ -72,6 +75,6 @@ class Rectangle:
 
         elif value < 0:
             raise ValueError("height must be >= 0")
-        
+
         else:
             self.__height = value
