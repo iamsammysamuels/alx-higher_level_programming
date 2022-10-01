01acf1580ed8c0dc6490bdcb10ea1d765753b55d
iamsammysamuels <iamsammysamuels@gmail.com>
2022-10-01 11:14:47 +0100

    Inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,44 +1,24 @@
 #!/usr/bin/python3
 
 """
-Created by Samuel Ezeh
-On the 30th of sept 2022
+Created by Samuel
+On the 1st of october 2022
 
-Implements the square class that inherits from the rectangle
+Implements the square class
 """
 
 
-rectangle = __import__("9-rectangle").Rectangle
+Rectangle = __import__("9-rectangle").Rectangle
 
-class Square(rectangle):
+class Square(Rectangle):
     """
-    A Square class shape, inheirts from
-    Rectangle and BaseGeometry
+    A Square class shape, inheirts from Rectangle and BaseGeometry
     """
     def __init__(self, size):
         """
-        initialises square class
+        Initialises the square class with size
 
         Attributes:
-            size (int): The size of the square
+            size (int): size of the square
         """
-        self.integer_validator("size", size)
-        self.__size = size
-
-    def __str__(self):
-        """
-        Implements the str function
-
-        Returns:
-            The str format of the instance
-        """
-        return "[Square] {}/{}".format(self.__size, self.__size)
-
-    def area(self):
-        """
-        Implements the area of the square
-
-        Returns:
-            The area of the square
-        """
-        return self.__size * self.__size
+        super().__init__(size, size)
