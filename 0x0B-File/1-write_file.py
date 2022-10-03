69ac05ac911b5e885d5727b79121b0b0fc08cdde
iamsammysamuels <iamsammysamuels@gmail.com>
2022-10-03 07:18:50 +0100

    Files

---
+++ 0x0B-File/1-write_file.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""A module created by Samuel Ezeh
+On the 28th of sept 2022
+Creates a function that writes a string to
+a text file (UTF8) and returns the number of characters written
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Function to write a string to a file
+
+    Args:
+
+        filename (str): the name of the file to write
+
+        text (str): the text to be written to the file
+
+    Return: The number of characters written
+    """
+    with open(filename, mode="w", encoding="UTF-8") as f:
+        char_len = f.write(text)
+        return (char_len)
--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
 
-"""A module created by Samuel Ezeh
-On the 28th of sept 2022
-THe module creates a function that reads
-a text file (UTF8) and prints it to stdout
-"""
-
-
 def read_file(filename=""):
-    """A function to read a file and print the contents"""
-    with open(filename, encoding="utf-8") as f:
-        read_f = f.read()
-        print(read_f, end="")
+    with open(filename, "r") as file:
+        print(file.read())
