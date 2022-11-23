3271c0bc78258c5b6762a33fabac07472b0570fa
iamsammysamuels <iamsammysamuels@gmail.com>
2022-11-23 20:06:26 +0100

    0x0E. SQL - More queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,2 @@
---A script that lists all privileges of user_0d_1 and user_0d_2
+-- A script that lists all privileges of user_0d_1 and user_0d_2
 CREATE USER IF NOT EXISTS "user_0d_1"@"localhost" IDENTIFIED BY "user_0d_1_pwd";
