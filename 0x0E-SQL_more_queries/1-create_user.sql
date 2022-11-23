5d6a50d66629d5ee73b45c9571ec0cce1c2b54a4
iamsammysamuels <iamsammysamuels@gmail.com>
2022-11-23 20:10:31 +0100

    Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,4 @@
 -- A script that lists all privileges of user_0d_1 and user_0d_2
-CREATE USER IF NOT EXISTS "user_0d_1"@"localhost" IDENTIFIED BY "user_0d_1_pwd";
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY "user_0d_1_pwd";
+-- Grant privileges
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
