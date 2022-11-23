a16597c66b1d52f5d87514936a25516603848d57
iamsammysamuels <iamsammysamuels@gmail.com>
2022-11-23 20:24:54 +0100

    Read user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- Create a database and user
+-- DDL to create a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- DDL create user
+CREATE USER IF NOT EXISTS "user_0d_2"@"localhost" IDENTIFIED BY 'user_0d_2_pwd';
