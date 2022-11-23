ae6e5f0b66cc61b67196a0dd630ce50144f6d583
iamsammysamuels <iamsammysamuels@gmail.com>
2022-11-23 19:54:14 +0100

    My privileges!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- A script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
-
+-- DML QUERY to list all privileges for user_0d_1 and user_0d_2 in localhost
+-- A script that lists all privileges using SHOW GRANTS
 SHOW GRANTS IF FOR "user_0d_1"@"localhoat";
 SHOW GRANTS FOR "user_0d_2"@"localhoat";
