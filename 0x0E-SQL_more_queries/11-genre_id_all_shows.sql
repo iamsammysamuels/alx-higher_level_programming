7781045d0c727aaecf39a1591153d15fa1ce687b
iamsammysamuels <iamsammysamuels@gmail.com>
2022-11-26 04:57:33 +0100

    Genre ID for all shows

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- lists all shows
+-- DML to list all shows contained in the database hbtn_0d_tvshow
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id
+ORDER BY title, genre_id ASC;
