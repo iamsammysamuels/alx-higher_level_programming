c6e00ed766755c92855e848669cc3d82a9a2c507
iamsammysamuels <iamsammysamuels@gmail.com>
2022-12-02 13:52:20 +0100

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,14 +7,14 @@ in the states table of hbtn_0e_0_usa where name matches the argument
 import MySQLdb
 from sys import argv
 
-if __name__ != "__main__":
-    return
-mydb = MySQLdb.connect(host="localhost", user=argv[1],
-                       db=argv[3], port=3306, passwd=argv[2])
-cur = mydb.cursor()
-cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY states.id".format(argv[4]))  # noqa
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-cur.close()
-mydb.close()
+if __name__ == "__main__":
+    mydb = MySQLdb.connect(host="localhost", user=argv[1],
+                       db=argv[3], port=3306, passwd=ar)
+    cur = mydb.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY\
+                '{:s}' ORDER BY states.id".format(argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    mydb.close()
