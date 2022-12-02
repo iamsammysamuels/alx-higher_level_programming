d5911ecf0dddc2c4107f0cbbdfe4d06194097314
iamsammysamuels <iamsammysamuels@gmail.com>
2022-12-02 11:11:22 +0100

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,15 +1,18 @@
-#!/usr/bin/env python3
-"""python script that lists all states from the database hbtn_0e_0_usa"""
+#!/usr/bin/python3
+"""
+python script that lists all states from the database hbtn_0e_0_usa
+"""
 
 import MySQLdb
 import sys
 
-mydb = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+if __name__ == "__main__":
+    mydb = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                        db=sys.argv[3], passwd=sys.argv[2])
-pointer = mydb.cursor()
-pointer.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")  # noqa
-rows = pointer.fetchall()
-for row in rows:
-    print(row)
-pointer.close()
-mydb.close()
+    pointer = mydb.cursor()
+    pointer.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")  # noqa
+    rows = pointer.fetchall()
+    for row in rows:
+        print(row)
+    pointer.close()
+    mydb.close()
